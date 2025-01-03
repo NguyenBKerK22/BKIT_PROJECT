4f74dc5c1531b3dcc96a960fe1b74463a13f1d32
KhiemNC <nguyencongkhiem510@gmail.com>
2025-01-03 23:24:35 +0700

    Define supported function for rs485-fsm

--- Core/Inc/rs485.h
+++ Core/Inc/rs485.h
@@ -13,10 +13,28 @@
 #include "global.h"
 #include "usart.h"
 
-void f_rs485_init(UART_HandleTypeDef *huart, uint8_t* receive_buffer, uint8_t* flag_rx, uint8_t* rx_size);
+void f_rs485_init(UART_HandleTypeDef *huart, uint8_t *receive_buffer, uint8_t *flag_rx, uint8_t *rx_size);
 
-void f_rs485_send_cmd(uint8_t* tx_buffer_without_crc, uint8_t tx_size_without_crc);
+void f_rs485_send_cmd(uint8_t *tx_buffer_without_crc, uint8_t tx_size_without_crc);
+
+void f_rs485_fsm_init();
 
 void f_rs485_fsm();
+
+void RS485_UART_Callback(UART_HandleTypeDef *huart);
+
+void _f_init_rs485_super_init();
+void _f_rs485_super_init();
+void _f_init_rs485_init();
+void _f_rs485_init();
+void _f_init_rs485_idle();
+void _f_rs485_idle();
+void _f_init_rs485_transmit();
+void _f_rs485_transmit();
+void _f_init_rs485_receive();
+void _f_rs485_receive();
+void _f_init_rs485_waiting_control();
+void _f_rs485_waiting_control();
+
 
 #endif /* INC_RS485_H_ */
--- Core/Src/rs485.c
+++ Core/Src/rs485.c
@@ -5,6 +5,36 @@
  *      Author: ADMIN
  */
 #include "rs485.h"
+#include "usart.h"
+#include "timer.h"
+#include "global.h"
+#include "crc16.h"
+
+enum modbus485_state_t
+{
+	RS485_SUPER_INIT,
+	RS485_INIT,
+	RS485_IDLE,
+	RS485_TRANSMIT,
+	RS485_RECEIVE,
+	RS485_WAITING_CONTROL
+
+};
+
+enum frame_status_t
+{
+	FRAME_OK,
+	FRAME_NOT_OK
+};
+
+UART_HandleTypeDef *_huart;
+uint8_t *receive_buffer;
+uint8_t *flag_rx;
+uint8_t *rx_size;
+uint8_t receive_index = 0;
+
+uint8_t transmit_buffer[256] = {0x00};
+uint8_t transmit_size = 0;
 
 /*
  * @brief Initialize values for variable include: huart channel, receive_buffer
@@ -15,7 +45,7 @@
  * @param rx_size: the size of receive command for callback fsm
  * @retval: None
  */
-void f_rs485_init(UART_HandleTypeDef *huart, uint8_t* receive_buffer, uint8_t* flag_rx, uint8_t* rx_size)
+void f_rs485_init(UART_HandleTypeDef *huart, uint8_t *receive_buffer, uint8_t *flag_rx, uint8_t *rx_size)
 {
 
 }
@@ -26,7 +56,12 @@ void f_rs485_init(UART_HandleTypeDef *huart, uint8_t* receive_buffer, uint8_t* f
  * @param tx_size_without_crc: number of bytes transmit, max is 254
  * @retval: None
  */
-void f_rs485_send_cmd(uint8_t* tx_buffer_without_crc, uint8_t tx_size_without_crc)
+void f_rs485_send_cmd(uint8_t *tx_buffer_without_crc, uint8_t tx_size_without_crc)
+{
+
+}
+
+void f_rs485_fsm_init()
 {
 
 }
@@ -36,6 +71,146 @@ void f_rs485_send_cmd(uint8_t* tx_buffer_without_crc, uint8_t tx_size_without_cr
  * @retval: None
  */
 void f_rs485_fsm()
+{
+	switch (MODBUS485_STATE) {
+		case RS485_SUPER_INIT:
+			_f_rs485_super_init();
+
+			if (1) // CHANGE STATE -> RS485_INIT
+			{
+				_f_init_rs485_init();
+				MODBUS485_STATE = RS485_INIT;
+			}
+			break;
+		case RS485_INIT:
+			_f_rs485_init();
+
+			if (isFlag(TI_RS485_T35_TIMER)) // CHANGE STATE -> RS485_IDLE
+			{
+				_f_init_rs485_idle();
+				MODBUS485_STATE = RS485_IDLE;
+			}
+			if (is_character_received()) // CHANGE STATE -> RS485_INIT
+			{
+				_f_init_rs485_init();
+				MODBUS485_STATE = RS485_INIT;
+			}
+			break;
+		case RS485_IDLE:
+			_f_rs485_idle();
+
+			if (is_transmit()) // CHANGE STATE -> RS485_TRANSMIT
+			{
+				HAL_GPIO_WritePin(RS485_EN_GPIO_Port, RS485_EN_Pin, 1);
+				HAL_UART_Transmit(&huart3, transmit_buffer, transmit_size, HAL_MAX_DELAY);
+				HAL_GPIO_WritePin(RS485_EN_GPIO_Port, RS485_EN_Pin, 0);
+				_f_init_rs485_transmit();
+				MODBUS485_STATE = RS485_TRANSMIT;
+			}
+			if (is_character_received()) // CHANGE STATE -> RS485_RECEIVE
+			{
+				_f_init_rs485_receive();
+				MODBUS485_STATE = RS485_RECEIVE;
+			}
+			break;
+		case RS485_TRANSMIT:
+			_f_rs485_transmit();
+
+			if (isFlag(TI_RS485_T35_TIMER)) // CHANGE STATE -> IDLE
+			{
+				_f_init_rs485_idle();
+				MODBUS485_STATE = RS485_IDLE;
+			}
+			break;
+		case RS485_RECEIVE:
+			_f_rs485_receive();
+
+			if (is_character_received()) // CHANGE STATE -> RS485_RECEIVE
+			{
+				_f_init_rs485_receive();
+				MODBUS485_STATE = RS485_RECEIVE;
+			}
+			if (isFlag(TI_RS485_T15_TIMER)) // CHANGE STATE -> RS485_WAITING_CONTROL
+			{
+				_f_init_rs485_waiting_control();
+				MODBUS485_STATE = RS485_WAITING_CONTROL;
+			}
+			break;
+		case RS485_WAITING_CONTROL:
+			_f_rs485_waiting_control();
+
+			if (isFlag(TI_RS485_T35_TIMER)) // CHANGE STATE -> RS485_IDLE
+			{
+				_f_init_rs485_idle();
+				MODBUS485_STATE = RS485_IDLE;
+			}
+			break;
+		default:
+			break;
+	}
+}
+
+void _f_init_rs485_super_init()
+{
+
+}
+
+void _f_rs485_super_init()
+{
+
+}
+
+void _f_init_rs485_init()
+{
+
+}
+
+void _f_rs485_init()
+{
+
+}
+
+void _f_init_rs485_idle()
+{
+
+}
+
+void _f_rs485_idle()
+{
+
+}
+
+void _f_init_rs485_transmit()
+{
+
+}
+
+void _f_rs485_transmit()
+{
+
+}
+
+void _f_init_rs485_receive()
+{
+
+}
+
+void _f_rs485_receive()
+{
+
+}
+
+void _f_init_rs485_waiting_control()
+{
+
+}
+
+void _f_rs485_waiting_control()
+{
+
+}
+
+void RS485_UART_Callback(UART_HandleTypeDef *huart)
 {
 
 }
